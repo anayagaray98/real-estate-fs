f1ce13e3048550f6bad674601ee0d212bcc33632
Carlos Anaya <carlos.anaya@aiterra.co>
2025-10-29 12:18:05 -0500

    Updating eslint configuration file

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,18 +1,57 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import pluginNext from '@next/eslint-plugin-next';
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+export default defineConfig([
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+      "__tests__/**",
+      "**/*.test.*",
+      "**/*.stories.*",
+    ],
+  },
+  // Base configs (recommended)
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  // General rules for all source files
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      '@next/next': pluginNext,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      // Next.js rules
+      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs['core-web-vitals'].rules,
+      "prefer-const": "error",
+      "no-var": "error",
+      eqeqeq: ["error", "smart"],
+      // Disable the console rule globally
+      "no-console": "off",
+      // Semicolon config
+      semi: "warn",
+      // TypeScript related configuration
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
 ]);
-
-export default eslintConfig;
